af97770ccb0b1863d4fb96db45720a1b5a0d5954
Hack Reactor Student <student@hackreactor.com>
2017-10-27 15:56:33 -0700

    finished video player

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,9 @@
 angular.module('video-player')
 
 .component('app', {
+  controller: function() {
+    this.currentVideo = window.exampleVideoData[0];
+    this.videos = window.exampleVideoData;
+  },
   templateUrl: '/src/templates/app.html'
 });
