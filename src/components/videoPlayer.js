d978170deca1998a4a7a53068e739d9f8eff5287
Hack Reactor Student <student@hackreactor.com>
2017-10-28 14:52:10 -0700

    Toggle autoplay working! Changed videoPlayer component to be stateful.

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -4,5 +4,11 @@ angular.module('video-player')
   bindings: {
     video: '<'
   },
+  controller: function() {
+    this.autoplay = 0;
+    this.toggleAutoplay = function() {
+      this.autoplay ? this.autoplay = 0 : this.autoplay = 1;
+    };
+  },
   templateUrl: 'src/templates/videoPlayer.html'
 });
