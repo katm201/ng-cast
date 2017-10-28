1cf1be46064ce51428ef804249112b2fc167f2a9
katm201 <katm201@gmail.com>
2017-10-27 20:34:23 -0700

    Youtube API hooked up and service functioning.

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 angular.module('video-player')
-
 .component('app', {
-  controller: function() {
+  controller: function(youTube) {
     this.currentVideo = window.exampleVideoData[0];
     this.videos = window.exampleVideoData;
-    this.handleSelection = video => {
+    this.selectVideo = video => {
       this.currentVideo = video;
     };
+    this.search = youTube.search;
   },
   templateUrl: 'src/templates/app.html'
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,22 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http) {
+  this.search = function(query) {
+    $http({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      method: 'GET',
+      params: {
+        key: window.YOUTUBE_API_KEY,
+        q: query,
+        type: 'video',
+        part: 'snippet',
+        maxResults: 5
+      }
+    })
+    .then(function successCallback (data) { 
+      console.log('success! ', data); 
+    }, 
+    function errorCallback(error) {
+      console.log('error! ', error);
+    });
+  };
 });
