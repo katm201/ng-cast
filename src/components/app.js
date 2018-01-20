7660a5e74ddd65b4b2dcaa86d5af81aec83450b8
Katherine Manning <katm201@gmail.com>
2018-01-19 16:09:19 -0800

    add youtube search

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,13 @@ angular.module('video-player')
     templateUrl: './src/templates/app.html',
     controller: function(youTube) {
       this.videos = window.exampleVideoData;
-      this.currentVideo = window.exampleVideoData[0];
+      this.currentVideo = this.videos[0];
       this.selectVideo = (video) => {
         this.currentVideo = video;
       };
-      this.searchVideos = youTube.search;
+      this.searchVideos = youTube.search.bind(this, (data) => {
+        this.videos = data;
+        this.selectVideo(data[0]);
+      });
     }
   });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,6 +1,22 @@
+const apiKey = window.YOUTUBE_API_KEY;
+
 angular.module('video-player')
-  .service('youTube', function() {
-    this.search = function(term) {
-      console.log(term);
+  .service('youTube', function($http) {
+    this.search = function(callback, term) {
+      $http({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        params: {
+          key: apiKey,
+          maxResults: '5',
+          part: 'snippet',
+          q: term,
+          type: ''
+        }
+      })
+        .then((response) => {
+          callback(response.data.items);
+        })
+        .catch((err) => { console.log(err); });
     };
   });
